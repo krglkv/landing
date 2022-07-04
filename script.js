af1a46b3dcec3553ecc97cf157c379b2b80365f0
Artem <senkone777@gmail.com>
2022-07-04 13:38:23 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// const cookies = document.querySelector('.cookies')
+// const cookiesBtn = document.querySelector('.coockies__button')
+// cookiesBtn.addEventListener('click', () => {
+//     cookies.style.display = 'none';
+// })
+
+// const coockiesVisible = () => {
+//     cookies.classList.add('cookies_visible')
+//     document.removeEventListener('DOMContentLoaded', coockiesVisible)
+// }
+// document.addEventListener('DOMContentLoaded', coockiesVisible)
+
+
+
+// const imgPhone = document.querySelector('.two__mobile-img');
+
+// const scrollPhone = () => {
+//     let leftPX = -800;
+//     imgPhone.style.left = leftPX + 'px'
+//     // console.log(window.scrollY)
+//     if (window.scrollY >= 700) {
+//         leftPX = window.scrollY - 700
+//     }
+// }
+
+// window.addEventListener('scroll', scrollPhone);
